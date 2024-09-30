db968d4c76d6fb6309714107feb94ca63ea3fff2
Roniel Pangan <roniel.pangan06@gmail.com>
2024-09-30 15:28:54 -0400

    Completed lab3b.py

--- test_import.py
+++ test_import.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import lab3a
+import lab3b
 
 text = lab3a.return_text_value()
 print(text)
 print(lab3a.return_number_value())
+
+
+print(lab3b.sum_numbers(10, 5))       # Should return 15
+print(lab3b.subtract_numbers(10, 5))   # Should return 5
+print(lab3b.multiply_numbers(10, 5))    # Should return 50
