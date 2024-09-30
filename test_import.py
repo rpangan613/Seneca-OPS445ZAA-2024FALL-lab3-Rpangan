952a884f7f6f792a932ff97ac8fad757a0d63113
Roniel Pangan <roniel.pangan06@gmail.com>
2024-09-30 16:30:58 -0400

    Completed lab3d.py

--- test_import.py
+++ test_import.py
@@ -2,15 +2,16 @@
 import lab3a
 import lab3b
 import lab3c
+import lab3d
 
 text = lab3a.return_text_value()
 print(text)
 print(lab3a.return_number_value())
 
 
-print(lab3b.sum_numbers(10, 5))       # Should return 15
-print(lab3b.subtract_numbers(10, 5))   # Should return 5
-print(lab3b.multiply_numbers(10, 5))    # Should return 50
+print(lab3b.sum_numbers(10, 5))
+print(lab3b.subtract_numbers(10, 5))
+print(lab3b.multiply_numbers(10, 5))
 
 
 print(lab3c.operate(10, 20, 'add'))
@@ -21,3 +22,6 @@ print(lab3c.operate(5, 5, 'multiply'))
 print(lab3c.operate(10, 100, 'multiply'))
 print(lab3c.operate(100, 5, 'divide'))
 print(lab3c.operate(100, 5, 'power'))
+
+
+print(lab3d.free_space())
